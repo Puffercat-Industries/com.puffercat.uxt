1e2e70c0cda4539f23ef98e3080e7dc444f42b71
Billy Wang <yunruiwa@usc.edu>
2023-06-30 11:01:13 +0800

    feat: extensions for serialized object

---
+++ Editor/EditorUI/SerializedObjectExtension.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+namespace Puffercat.Uxt.Editor.EditorUI
+{
+    public static class SerializedObjectExtension
+    {
+        public static VisualElement CreatePropertyDrawer(this SerializedObject so, string propertyName)
+        {
+            var property = so.FindProperty(propertyName);
+            return new PropertyField(so.FindProperty(propertyName));
+        }
+
+        public static IEnumerable<VisualElement> CreatePropertyDrawersForAllChildren(this SerializedProperty property)
+        {
+            foreach (SerializedProperty child in property)
+            {
+                if (child is null) continue;
+                yield return new PropertyField(child);
+            }
+        }
+    }
+}
