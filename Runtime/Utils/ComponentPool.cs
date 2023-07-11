6fdc2e4e43debaa85d53c4ac948b7018f5aa0c02
Billy Wang <yunruiwa@usc.edu>
2023-07-11 21:56:25 +0800

    feat: component object pool

---
+++ Runtime/Utils/ComponentPool.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Puffercat.Uxt.Utils
+{
+    public static class ComponentPoolStatics
+    {
+        private static RectTransform s_rectTransformRoot;
+        
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        private static void StaticInit()
+        {
+            s_rectTransformRoot = null;
+        }
+
+        public static RectTransform TransformRoot
+        {
+            get
+            {
+                if (s_rectTransformRoot is null)
+                {
+                    var go = new GameObject("[Component Pool Root]");
+                    s_rectTransformRoot = go.AddComponent<RectTransform>();
+                    Object.DontDestroyOnLoad(s_rectTransformRoot);
+                }
+
+                return s_rectTransformRoot;
+            }
+        }
+    }
+
+
+    public class ComponentPool<T> : ScriptableObject where T : Component
+    {
+        [SerializeField] private T m_prefab;
+    }
+}
---
+++ Runtime/Utils/SimpleObjectPool.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Puffercat.Uxt.Utils
+{
+    [ExecuteAlways]
+    public class SimpleObjectPool<T> : MonoBehaviour where T : Component
+    {
+        [SerializeField] private T m_prefab;
+        private List<T> m_pool;
+
+        private void Awake()
+        {
+            m_pool = new List<T>();
+        }
+
+        public T Allocate(Transform parent = null, bool worldPositionStays = false)
+        {
+            T component;
+
+            if (m_pool.Count == 0)
+            {
+                component = Instantiate(m_prefab, parent, worldPositionStays);
+            }
+            else
+            {
+                component = m_pool[^1];
+                component.gameObject.SetActive(true);
+                component.transform.SetParent(parent, worldPositionStays);
+                m_pool.RemoveAt(m_pool.Count - 1);
+            }
+
+            if (!Application.IsPlaying(this))
+            {
+                component.gameObject.hideFlags = HideFlags.DontSaveInBuild | HideFlags.DontSaveInEditor;
+            }
+
+            SetUpObject(component);
+
+            return component;
+        }
+
+        public void Free(T component)
+        {
+            Debug.Assert(!m_pool.Contains(component), "!m_pool.Contains(component)");
+
+            if (!Application.IsPlaying(this))
+            {
+                component.gameObject.hideFlags = HideFlags.DontSaveInBuild | HideFlags.DontSaveInEditor;
+            }
+
+            component.gameObject.SetActive(false);
+            component.transform.SetParent(transform, false);
+            m_pool.Add(component);
+        }
+
+        public void FreeList(IList<T> components)
+        {
+            Debug.Assert(components != null, "components != null");
+            foreach (var component in components)
+            {
+                Free(component);
+            }
+            components.Clear();
+        }
+
+        protected virtual void SetUpObject(T obj)
+        {
+        }
+
+        protected virtual void TearDownObject(T obj)
+        {
+        }
+    }
+}
